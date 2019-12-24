fb6c376baf4e59061921974c8a67424acb501ef8
袁恩志 <710575900@qq.com>
2019-12-25 00:36:43 +0800

    ``

--- webmvc-modules/webmvc-mybatis-base/src/main/java/org/yuan/boot/webmvc/pojo/PageResult.java
+++ webmvc-modules/webmvc-mybatis-base/src/main/java/org/yuan/boot/webmvc/pojo/PageResult.java
@@ -1,10 +1,11 @@
 package org.yuan.boot.webmvc.pojo;
 
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class PageResult<T> extends PageIterableResult<T> {
-
+    
 }
