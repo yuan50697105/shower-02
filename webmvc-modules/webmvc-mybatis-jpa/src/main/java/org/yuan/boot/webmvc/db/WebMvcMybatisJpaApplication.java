4b9acd2a0f1a443b6618cb3a77cb44dceb2e0aa6
袁恩志 <710575900@qq.com>
2019-12-26 20:36:25 +0800

    feat: 新模块
    - WebMvcMybatisJpaApplication
    - WebMvcMybatisJpaSqlHelperApplication

---
+++ webmvc-modules/webmvc-mybatis-jpa/src/main/java/org/yuan/boot/webmvc/db/WebMvcMybatisJpaApplication.java
@@ -0,0 +1,11 @@
+package org.yuan.boot.webmvc.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcMybatisJpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebMvcMybatisJpaApplication.class, args);
+    }
+}
