2a407282885bed534fa3161f65b998e8e833b598
袁恩志 <710575900@qq.com>
2019-12-29 16:29:27 +0800

    重构代码

---
+++ db-modules/db-jpa-mybatis-mapper-plus/src/main/java/org/yuan/boot/DbJpaMybatisMapperPlusApplication.java
@@ -0,0 +1,11 @@
+package org.yuan.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbJpaMybatisMapperPlusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DbJpaMybatisMapperPlusApplication.class, args);
+    }
+}
---
+++ db-modules/db-jpa-mybatis-plus/src/main/java/org/yuan/boot/db/mapper/BaseMapper.java
@@ -0,0 +1,4 @@
+package org.yuan.boot.db.mapper;
+
+public interface BaseMapper<T> extends com.baomidou.mybatisplus.core.mapper.BaseMapper<T> {
+}
