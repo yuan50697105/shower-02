e10303369613a25288dec4088f88afa31045c04f
袁恩志 <710575900@qq.com>
2020-01-01 21:57:38 +0800

    ...

--- buildCopyJarToDesktop.bat
+++ buildCopyJarToDesktop.bat
@@ -1,10 +1,10 @@
 echo off
-gradlew clean build
-mkdir %USERPROFILE%\desktop\gradle
-mkdir %USERPROFILE%\desktop\gradle\exe
-mkdir %USERPROFILE%\desktop\gradle\lib
-mkdir %USERPROFILE%\desktop\gradle\all
-for /r . %%i in (*exec.jar) do xcopy /q /a %%i %USERPROFILE%\desktop\gradle\exe /y
-for /r . %%i in (*dev.jar) do xcopy /q /a %%i %USERPROFILE%\desktop\gradle\lib /y
-for /r . %%i in (*dev*.jar) do xcopy /q /a %%i %USERPROFILE%\desktop\gradle\all /y
-gradlew clean
+call gradlew clean build
+call mkdir %USERPROFILE%\desktop\gradle
+call mkdir %USERPROFILE%\desktop\gradle\exe
+call mkdir %USERPROFILE%\desktop\gradle\lib
+call mkdir %USERPROFILE%\desktop\gradle\all
+for /r . %%i in (*exec.jar) do call xcopy /q /a %%i %USERPROFILE%\desktop\gradle\exe /y
+for /r . %%i in (*dev.jar) do call xcopy /q /a %%i %USERPROFILE%\desktop\gradle\lib /y
+for /r . %%i in (*dev*.jar) do call xcopy /q /a %%i %USERPROFILE%\desktop\gradle\all /y
+call gradlew clean
