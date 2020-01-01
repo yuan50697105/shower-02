05aea7b60784d972c267c8a5b9567f7f1b4f5069
袁恩志 <710575900@qq.com>
2020-01-01 18:54:27 +0800

    modify: 修改依赖

--- webmvc-app-project/webmvc-app-project-03/src/main/java/org/yuan/boot/webmvc/app/WebMvcApplication03.java
+++ webmvc-app-project/webmvc-app-project-03/src/main/java/org/yuan/boot/webmvc/app/WebMvcApplication03.java
@@ -1,10 +1,12 @@
 package org.yuan.boot.webmvc.app;
 
+import org.minbox.framework.api.boot.autoconfigure.swagger.annotation.EnableApiBootSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.yuan.boot.DbCoreApplication;
 import org.yuan.boot.db.DbMybatisMapperApplication;
 import org.yuan.boot.webmvc.WebMvcCoreApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @program: learning-demo-02
@@ -12,6 +14,8 @@ import org.yuan.boot.webmvc.WebMvcCoreApplication;
  * @author: yuane
  * @create: 2019-12-29 22:51
  */
+@EnableSwagger2
+@EnableApiBootSwagger
 @SpringBootApplication(scanBasePackageClasses = {WebMvcCoreApplication.class, DbCoreApplication.class, DbMybatisMapperApplication.class})
 public class WebMvcApplication03 {
     public static void main(String[] args) {
--- webmvc-app-project/webmvc-app-project-11/src/main/java/org/yuan/boot/webmvc/app/WebMvcApplication11.java
+++ webmvc-app-project/webmvc-app-project-11/src/main/java/org/yuan/boot/webmvc/app/WebMvcApplication11.java
@@ -1,10 +1,12 @@
 package org.yuan.boot.webmvc.app;
 
+import org.minbox.framework.api.boot.autoconfigure.swagger.annotation.EnableApiBootSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.yuan.boot.DbCoreApplication;
 import org.yuan.boot.DbJpaMybatisMapperApplication;
 import org.yuan.boot.webmvc.WebMvcCoreApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @program: learning-demo-02
@@ -12,6 +14,8 @@ import org.yuan.boot.webmvc.WebMvcCoreApplication;
  * @author: yuane
  * @create: 2019-12-29 23:37
  */
+@EnableSwagger2
+@EnableApiBootSwagger
 @SpringBootApplication(scanBasePackageClasses = {WebMvcCoreApplication.class, DbCoreApplication.class, DbJpaMybatisMapperApplication.class})
 public class WebMvcApplication11 {
     public static void main(String[] args) {
