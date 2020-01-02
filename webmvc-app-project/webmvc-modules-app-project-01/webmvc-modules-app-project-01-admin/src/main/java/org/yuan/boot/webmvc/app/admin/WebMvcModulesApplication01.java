ea7aa12d8633a3240fbf6f42e77745e98cb7f50b
yuanenzhi <710575900@qq.com>
2020-01-02 12:31:21 +0800

    ...

--- webmvc-app-project/webmvc-modules-app-project-01/webmvc-modules-app-project-01-admin/src/main/java/org/yuan/boot/webmvc/app/admin/WebMvcModulesApplication01.java
+++ webmvc-app-project/webmvc-modules-app-project-01/webmvc-modules-app-project-01-admin/src/main/java/org/yuan/boot/webmvc/app/admin/WebMvcModulesApplication01.java
@@ -2,17 +2,16 @@ package org.yuan.boot.webmvc.app.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.yuan.boot.webmvc.app.db.WebMvcModulesDbApplication01;
 
 /**
  * @program: learning-demo-02
  * @description:
- * @author: yuane
- * @create: 2020-01-01 19:57
- */
+ * @author: yuanez
+ * @create: 2020-01-02 12:29
+ **/
 @SpringBootApplication
-public class WebMvcModulesAdminApplication01 {
+public class WebMvcModulesApplication01 {
     public static void main(String[] args) {
-        SpringApplication.run(WebMvcModulesDbApplication01.class, args);
+        SpringApplication.run(WebMvcModulesApplication01.class, args);
     }
 }
