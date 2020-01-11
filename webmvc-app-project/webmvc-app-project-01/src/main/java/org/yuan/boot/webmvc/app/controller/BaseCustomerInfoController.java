7b387f5950575cb386ee38c56bff22472b8405c3
袁恩志 <710575900@qq.com>
2020-01-11 23:10:16 +0800

    ...

---
+++ webmvc-app-project/webmvc-app-project-01/src/main/java/org/yuan/boot/webmvc/app/controller/BaseCustomerInfoController.java
@@ -0,0 +1,22 @@
+package org.yuan.boot.webmvc.app.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yuan.boot.webmvc.app.service.BaseCustomerInfoService;
+import org.yuan.boot.webmvc.controller.ResultController;
+
+/**
+ * @program: learning-demo-02
+ * @description:
+ * @author: yuane
+ * @create: 2020-01-11 23:09
+ */
+@RestController
+@RequestMapping("base/customer/info")
+@AllArgsConstructor
+public class BaseCustomerInfoController extends ResultController {
+    @Autowired
+    private BaseCustomerInfoService baseCustomerInfoService;
+}
