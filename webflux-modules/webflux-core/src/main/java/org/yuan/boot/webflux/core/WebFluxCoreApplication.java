a09a2648a8a70c7dcfd35fc3033873e16314ebe1
袁恩志 <710575900@qq.com>
2020-01-11 18:44:30 +0800

    ...

---
+++ webflux-modules/webflux-core/src/main/java/org/yuan/boot/webflux/core/WebFluxCoreApplication.java
@@ -0,0 +1,17 @@
+package org.yuan.boot.webflux.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: learning-demo-02
+ * @description:
+ * @author: yuane
+ * @create: 2020-01-11 18:37
+ */
+@SpringBootApplication
+public class WebFluxCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxCoreApplication.class, args);
+    }
+}
