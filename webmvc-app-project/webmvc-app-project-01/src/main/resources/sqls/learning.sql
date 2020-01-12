e183e736a9565d401009faeda088e131d53917de
袁恩志 <710575900@qq.com>
2020-01-13 00:37:53 +0800

    ...

--- webmvc-app-project/webmvc-app-project-01/src/main/resources/sqls/learning.sql
+++ webmvc-app-project/webmvc-app-project-01/src/main/resources/sqls/learning.sql
@@ -45,15 +45,12 @@ create table if not exists sys_permission
 (
     id          bigint(15) auto_increment comment 'id'
         primary key,
-    authority   varchar(50) null comment 'authority',
     name        varchar(50) null comment 'name',
     create_user varchar(50) null comment 'createUser',
     update_user varchar(50) null comment 'updateUser',
     create_time datetime    null comment 'createTime',
     update_time timestamp   null comment 'updateTime',
     enabled     int         null comment 'enabled',
-    constraint authority
-        unique (authority),
     constraint name
         unique (name)
 )
@@ -63,15 +60,12 @@ create table if not exists sys_role
 (
     id          bigint(15) auto_increment comment 'id'
         primary key,
-    authority   varchar(50) null comment 'authority',
     name        varchar(50) null comment 'name',
     create_user varchar(50) null comment 'createUser',
     update_user varchar(50) null comment 'updateUser',
     create_time datetime    null comment 'createTime',
     update_time timestamp   null comment 'updateTime',
     enabled     int         null comment 'enabled',
-    constraint authority
-        unique (authority),
     constraint name
         unique (name)
 )
@@ -129,9 +123,7 @@ select `su`.`id`          AS `id`,
        `su`.`update_time` AS `update_time`,
        `su`.`enabled`     AS `enabled`,
        `sr`.`name`        AS `role_name`,
-       `sr`.`authority`   AS `role_authority`,
-       `sp`.`name`        AS `permission_name`,
-       `sp`.`authority`   AS `permission_authority`
+       `sp`.`name`        AS `permission_name`
 from ((((`learning`.`sys_user` `su` left join `learning`.`sys_user_role` `sur` on ((`sur`.`user_id` = `su`.`id`))) left join `learning`.`sys_role` `sr` on ((`sr`.`id` = `sur`.`role_id`))) left join `learning`.`sys_role_permission` `srp` on ((`srp`.`role_id` = `sr`.`id`)))
          left join `learning`.`sys_permission` `sp` on ((`sp`.`id` = `srp`.`permission_id`)));
 
@@ -157,11 +149,7 @@ from ((((`learning`.`sys_user` `su` left join `learning`.`sys_user_role` `sur` o
 
 -- comment on column sys_user_role_permission_view.role_name not supported: name
 
--- comment on column sys_user_role_permission_view.role_authority not supported: authority
-
 -- comment on column sys_user_role_permission_view.permission_name not supported: name
-
--- comment on column sys_user_role_permission_view.permission_authority not supported: authority
 
 create or replace definer = root@localhost view sys_user_role_view as
 select `user`.`id`          AS `id`,
@@ -174,7 +162,6 @@ select `user`.`id`          AS `id`,
        `user`.`create_time` AS `create_time`,
        `user`.`update_time` AS `update_time`,
        `user`.`enabled`     AS `enabled`,
-       `role`.`authority`   AS `authority`,
        `role`.`name`        AS `role_name`
 from ((`learning`.`sys_user` `user` left join `learning`.`sys_user_role` `user_role` on ((`user_role`.`user_id` = `user`.`id`)))
          left join `learning`.`sys_role` `role` on ((`role`.`id` = `user_role`.`role_id`)));
@@ -198,8 +185,6 @@ from ((`learning`.`sys_user` `user` left join `learning`.`sys_user_role` `user_r
 -- comment on column sys_user_role_view.update_time not supported: updateTime
 
 -- comment on column sys_user_role_view.enabled not supported: enabled
-
--- comment on column sys_user_role_view.authority not supported: authority
 
 -- comment on column sys_user_role_view.role_name not supported: name
 
