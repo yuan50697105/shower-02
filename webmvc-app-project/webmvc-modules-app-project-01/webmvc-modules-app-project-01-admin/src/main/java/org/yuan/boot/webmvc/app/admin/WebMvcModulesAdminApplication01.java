7e01d6f0a01a4916e27089a485d08a5f7185df01
袁恩志 <710575900@qq.com>
2020-01-12 17:31:11 +0800

    ...

--- webmvc-app-project/webmvc-modules-app-project-01/webmvc-modules-app-project-01-admin/src/main/java/org/yuan/boot/webmvc/app/admin/WebMvcModulesAdminApplication01.java
+++ webmvc-app-project/webmvc-modules-app-project-01/webmvc-modules-app-project-01-admin/src/main/java/org/yuan/boot/webmvc/app/admin/WebMvcModulesAdminApplication01.java
@@ -1,7 +1,12 @@
 package org.yuan.boot.webmvc.app.admin;
 
+import org.minbox.framework.api.boot.autoconfigure.swagger.annotation.EnableApiBootSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.yuan.boot.DbCoreApplication;
+import org.yuan.boot.DbMybatisBaseApplication;
+import org.yuan.boot.webmvc.WebMvcCoreApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @program: learning-demo-02
@@ -9,7 +14,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author: yuane
  * @create: 2020-01-01 19:57
  */
-@SpringBootApplication
+@EnableSwagger2
+@EnableApiBootSwagger
+@SpringBootApplication(scanBasePackageClasses = {WebMvcCoreApplication.class, DbCoreApplication.class, DbMybatisBaseApplication.class})
 public class WebMvcModulesAdminApplication01 {
     public static void main(String[] args) {
         SpringApplication.run(WebMvcModulesAdminApplication01.class, args);
--- webmvc-app-project/webmvc-modules-app-project-01/webmvc-modules-app-project-01-device/src/main/java/org/yuan/boot/webmvc/app/wx/WebMvcModuleDeviceApplication01.java
+++ webmvc-app-project/webmvc-modules-app-project-01/webmvc-modules-app-project-01-device/src/main/java/org/yuan/boot/webmvc/app/wx/WebMvcModuleDeviceApplication01.java
@@ -1,4 +1,4 @@
-package org.yuan.boot.webmvc.app;
+package org.yuan.boot.webmvc.app.wx;
 
 import org.minbox.framework.api.boot.autoconfigure.swagger.annotation.EnableApiBootSwagger;
 import org.springframework.boot.SpringApplication;
@@ -12,13 +12,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @program: learning-demo-02
  * @description:
  * @author: yuane
- * @create: 2020-01-01 19:47
+ * @create: 2020-01-12 17:03
  */
 @EnableSwagger2
 @EnableApiBootSwagger
 @SpringBootApplication(scanBasePackageClasses = {WebMvcCoreApplication.class, DbCoreApplication.class, DbMybatisBaseApplication.class})
-public class WebMvcModulesDbApplication01 {
+public class WebMvcModuleDeviceApplication01 {
     public static void main(String[] args) {
-        SpringApplication.run(WebMvcModulesDbApplication01.class, args);
+        SpringApplication.run(WebMvcModuleDeviceApplication01.class, args);
     }
 }
