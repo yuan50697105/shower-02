2ed9c5c215306619eeab458f2b26971e5a05e496
yuanenzhi <710575900@qq.com>
2020-01-14 14:59:54 +0800

    ...

--- sysc-dev.bat
+++ sysc-dev.bat
@@ -1,6 +1,6 @@
 for %%i in (1,2,3,4,5,6,7,8) do (
-    for %%a in (webmvc-app-project-0,webmvc-app-project-jpa-0,webmvc-module-app-project-0,webmvc-module-app-project-jpa-0) do (
-        call git checkout -b %%a%%i 
+    for %%a in (dev-app-0,dev-app-jpa-0,dev-module-app-0,dev-module-app-jpa-0) do (
+        call git switch %%a%%i 
         call git pull origin dev
     )
 )
