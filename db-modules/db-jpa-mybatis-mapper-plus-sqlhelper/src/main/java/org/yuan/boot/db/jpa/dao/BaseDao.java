c9ded2dc9bb21147c75c20a823a7c91bcb8baca2
yuanenzhi <710575900@qq.com>
2020-01-16 13:51:07 +0800

    删除无用结构
    删除无用结构

--- db-modules/db-jpa-mybatis-mapper-plus-sqlhelper/src/main/java/org/yuan/boot/db/jpa/dao/BaseDao.java
+++
@@ -1,8 +0,0 @@
-package org.yuan.boot.db.jpa.dao;
-
-import org.yuan.boot.db.jpa.pojo.AbstractBaseEntity;
-
-import java.io.Serializable;
-
-public interface BaseDao<T extends AbstractBaseEntity<T>, ID extends Serializable> {
-}
--- db-modules/db-jpa-mybatis-plus-sqlhelper/src/main/java/org/yuan/boot/db/jpa/mapper/BaseMapper.java
+++
@@ -1,4 +0,0 @@
-package org.yuan.boot.db.jpa.mapper;
-
-public interface BaseMapper<T> extends com.baomidou.mybatisplus.core.mapper.BaseMapper<T> {
-}
