a1df114d45f668cc34169b5335cc80ff51893c0c
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-01-17 20:11:40 +0800

    Update buildCopyJarToDesktop.bat

--- buildCopyJarToDesktop.bat
+++ buildCopyJarToDesktop.bat
@@ -2,8 +2,8 @@
 call gradlew clean build
 call mkdir %USERPROFILE%\desktop\gradle
 call mkdir %USERPROFILE%\desktop\gradle\exe
-call mkdir %USERPROFILE%\desktop\gradle\lib
+REM call mkdir %USERPROFILE%\desktop\gradle\lib
 for /r . %%i in (*exec.jar) do call xcopy /q /a %%i %USERPROFILE%\desktop\gradle\exe /y
-for /r . %%i in (*dev.jar) do call xcopy /q /a %%i %USERPROFILE%\desktop\gradle\lib /y
+REM for /r . %%i in (*dev.jar) do call xcopy /q /a %%i %USERPROFILE%\desktop\gradle\lib /y
 call gradlew clean
 call gradlew --stop 
