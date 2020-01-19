e5692bea189785cb72305b32f523fb633d06040b
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-01-19 23:16:55 +0800

    ShowerApplication02 修改结构

--- shower-02/src/main/java/org/yuan/boot/shower/interceptor/annotation/CreateTime.java
+++ shower-02/src/main/java/org/yuan/boot/shower/interceptor/annotation/CreateTime.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 创建时间注释
+ * @author yuane
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD})
 public @interface CreateTime {
--- shower-02/src/main/java/org/yuan/boot/shower/interceptor/annotation/Id.java
+++ shower-02/src/main/java/org/yuan/boot/shower/interceptor/annotation/Id.java
@@ -6,6 +6,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 主键注释
+ * @author yuane
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD})
 public @interface Id {
