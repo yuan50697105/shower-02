e97c573bd82a336ea20c1ea52bc237594e3a87c8
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-01-19 21:56:12 +0800

    修改结构

--- shower-01/src/main/java/org/yuan/boot/shower/ShowerApplication01.java
+++ shower-01/src/main/java/org/yuan/boot/shower/ShowerApplication01.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.minbox.framework.api.boot.autoconfigure.swagger.annotation.EnableApiBootSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.yuan.boot.DbMybatisBaseApplication;
 import org.yuan.boot.webmvc.WebMvcCoreApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @EnableSwagger2
 @EnableApiBootSwagger
+@EnableWebSecurity
 @SpringBootApplication(scanBasePackageClasses = {WebMvcCoreApplication.class, DbMybatisBaseApplication.class})
 @Log4j2
 public class ShowerApplication01 {
