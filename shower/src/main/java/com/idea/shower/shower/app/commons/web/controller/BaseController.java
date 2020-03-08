bcc7c0126b34b4deb196c14c2923989856ccf723
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-03-08 15:33:37 +0800

    修改结构

--- shower/src/main/java/com/idea/shower/shower/app/commons/web/controller/BaseController.java
+++ shower/src/main/java/com/idea/shower/shower/app/commons/web/controller/BaseController.java
@@ -9,4 +9,10 @@ import com.idea.shower.web.webmvc.controller.ResultController;
  * @create: 2020-01-31 09:39
  */
 public abstract class BaseController extends ResultController {
+    public final static String HTTP_METHOD_POST = "POST";
+    public final static String HTTP_METHOD_GET = "GET";
+    public final static String HTTP_METHOD_DELETE = "DELETE";
+    public final static String HTTP_METHOD_PUT = "PUT";
+    public final static String APPLICATION_JSON_UTF8 = "application/problem+json;charset=UTF-8";
+
 }
