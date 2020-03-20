40daafa22129ba0089da807caa71dcfa58a6175d
yuanenzhi <710575900@qq.com>
2020-03-20 10:31:47 +0800

    修改创建订单过程

---
+++ shower/src/main/java/com/idea/shower/app/ShowerApplication.java
@@ -0,0 +1,11 @@
+package com.idea.shower.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShowerApplication.class, args);
+    }
+}
