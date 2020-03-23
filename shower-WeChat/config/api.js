8a089af1832dae968977b6bcce42d1273033a329
18238593848 <wcq0508@163.com>
2020-03-23 11:14:05 +0800

    feat:小程序登录功能初始版

--- shower-WeChat/config/api.js
+++ shower-WeChat/config/api.js
@@ -1,9 +1,8 @@
 // 以下是业务服务器API地址
-var WxApiRoot = 'http://yuan50697105.nat300.top/api/wechat/';
-var WxApiRoot = 'http://localhost:8002/api/wechat/';
+var WxApiRoot = 'http://localhost:8002/wx/';
 module.exports = {
    //微信登录
-  AuthLoginByWeixin: WxApiRoot + 'auth/login_by_weixin',
+  AuthLoginByWeixin: WxApiRoot + 'customer/login',
   orderCreat: WxApiRoot + 'order/create',
   orderOver: WxApiRoot + 'order/stop',
   loginUrl: WxApiRoot + 'customer/get_openid'
