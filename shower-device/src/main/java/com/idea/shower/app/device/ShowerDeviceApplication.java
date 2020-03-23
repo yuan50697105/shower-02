e9caebb287c48474e0e177ae88e388f37b495a00
yuanenzhi <710575900@qq.com>
2020-03-23 11:04:08 +0800

    修复错误，访问路径404

--- shower-device/src/main/java/com/idea/shower/app/device/ShowerDeviceApplication.java
+++ shower-device/src/main/java/com/idea/shower/app/device/ShowerDeviceApplication.java
@@ -1,9 +1,11 @@
 package com.idea.shower.app.device;
 
+import com.idea.shower.app.db.ShowerDbApplication;
+import com.idea.shower.web.webmvc.WebMvcCoreApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {ShowerDeviceApplication.class, ShowerDbApplication.class, WebMvcCoreApplication.class})
 public class ShowerDeviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShowerDeviceApplication.class, args);
