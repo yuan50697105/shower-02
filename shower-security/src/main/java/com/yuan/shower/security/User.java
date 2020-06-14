cb631b5dceb630f5597e86393dbb60b7be8ed073
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-14 20:02:42 +0800

    升级版本

---
+++ shower-security/src/main/java/com/yuan/shower/security/User.java
@@ -0,0 +1,29 @@
+package com.yuan.shower.security;
+
+import lombok.*;
+
+import java.util.List;
+
+/**
+ * @program: boot-01
+ * @description:
+ * @author: yuane
+ * @create: 2020-05-31 16:07
+ */
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class User {
+    @NonNull
+    private Long id;
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
+    @NonNull
+    private List<String> roles;
+
+    private List<String> permissions;
+
+}
