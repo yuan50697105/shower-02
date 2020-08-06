7ac127a8c1ff0d79ec4984a144dd53438aef39c0
yuan50697105 <710575900@qq.com>
2020-08-06 21:29:35 +0800

    通用转换器

--- shower-admin/src/main/java/com/idea/shower/admin/admin/converter/AdminEntityConverter.java
+++ shower-admin/src/main/java/com/idea/shower/admin/admin/converter/AdminEntityConverter.java
@@ -1,9 +1,6 @@
 package com.idea.shower.admin.admin.converter;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.NullValueCheckStrategy;
-import org.mapstruct.NullValueMappingStrategy;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 
 /**
  * @program: shower-01
@@ -11,7 +8,7 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
  * @author: yuane
  * @create: 2020-07-19 22:49
  */
-@Mapper(componentModel = "spring", nullValueMappingStrategy = NullValueMappingStrategy.RETURN_NULL, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+@Mapper(componentModel = "spring")
 public interface AdminEntityConverter {
 
 
