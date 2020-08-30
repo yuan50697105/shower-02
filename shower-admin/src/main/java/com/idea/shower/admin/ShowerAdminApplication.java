6fb866e3f17ff27cbe8c4dbad3e9dd6702eab356
yuan50697105 <710575900@qq.com>
2020-08-30 13:00:01 +0800

    fix:vue recall twice

--- shower-admin/src/main/java/com/idea/shower/admin/ShowerAdminApplication.java
+++ shower-admin/src/main/java/com/idea/shower/admin/ShowerAdminApplication.java
@@ -1,14 +1,12 @@
 package com.idea.shower.admin;
 
 import cn.hutool.extra.spring.SpringUtil;
-import com.idea.shower.commons.ShowerCommons;
-import com.idea.shower.web.webmvc.WebMvcCoreApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackageClasses = {ShowerAdminApplication.class, ShowerDbApplication.class, WebMvcCoreApplication.class, DbCoreApplication.class, ShowerCommons.class})
+@SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @Import(SpringUtil.class)
 public class ShowerAdminApplication {
