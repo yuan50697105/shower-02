880cf8db31d2d29291473804ccd6065649808e5e
finch <wcq0508@163.com>
2020-09-07 14:22:36 +0800

    fix:修改小程序及设备详情接口

--- shower-WeChat/config/api.js
+++ shower-WeChat/config/api.js
@@ -1,16 +1,25 @@
 // 以下是业务服务器API地址
-// var WxApiRoot = 'http://123.56.78.153:8002/wx/';
-var WxApiRoot = 'https://yuan50697105s.natapp4.cc/wx/';
+var WxApiRoot = 'http://127.0.0.1:8002/wx/';
+// var WxApiRoot = 'https://yuan50697105s.natapp4.cc/wx/';
 module.exports = {
    //微信登录
   AuthLoginByWeixin: WxApiRoot + 'customer/login',
+  //区域列表
   AreaList: WxApiRoot + 'area/areas',
+  //设备列表
   DeviceInfoList: WxApiRoot + 'device/deviceInfoList',
-  DeviceInfoDetail: WxApiRoot + 'device/send/info',
+  //设备详情
+  DeviceInfoDetail: WxApiRoot + 'device/info',
+  //添加订单
   AddOrder: WxApiRoot + 'order/add',
+  //订单列表
   OrderList: WxApiRoot + 'order/data',
+  //开始订单
   StartOrder: WxApiRoot + 'order/open',
+  //结束订单
   EndOrder: WxApiRoot + 'order/end',
+  //订单详情
   OrderDetail: WxApiRoot + 'order/item',
+  //付款
   OrderPay: WxApiRoot + 'order/pay',
 };
