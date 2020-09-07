1d412fadb346618e0eeafea7534f1ea953f5409e
finch <wcq0508@163.com>
2020-09-07 10:50:30 +0800

    fix:修改小程序data.data

--- shower-WeChat/config/api.js
+++ shower-WeChat/config/api.js
@@ -1,6 +1,6 @@
 // 以下是业务服务器API地址
 // var WxApiRoot = 'http://123.56.78.153:8002/wx/';
-var WxApiRoot = 'http://127.0.0.1:8002/wx/';
+var WxApiRoot = 'https://yuan50697105s.natapp4.cc/wx/';
 module.exports = {
    //微信登录
   AuthLoginByWeixin: WxApiRoot + 'customer/login',
