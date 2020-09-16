c08f6c701b7d9b25537bbd2efb8efb751eba857a
finch <wcq0508@163.com>
2020-09-16 16:54:04 +0800

    fix:修改PC上框颜色

--- shower-admin-vue/src/layout/components/Navbar.vue
+++ shower-admin-vue/src/layout/components/Navbar.vue
@@ -87,7 +87,7 @@ export default {
   height: 50px;
   overflow: hidden;
   position: relative;
-  background: #fff;
+  background: #1890FF;
   box-shadow: 0 1px 4px rgba(0,21,41,.08);
 
   .hamburger-container {
