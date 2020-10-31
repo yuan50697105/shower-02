bff554c15766beacc9805acb399a6f8a089c6687
yuan50697105 <710575900@qq.com>
2020-10-31 09:43:58 +0800

    [样式修改]: 楼宇信息，查看功能删除

--- shower-admin-vue/src/views/area/areabuilding.vue
+++ shower-admin-vue/src/views/area/areabuilding.vue
@@ -26,7 +26,7 @@
         <el-table-column prop="areaName" label="所属区域" header-align="center" align="center" />
         <el-table-column :label="$t('handle')" fixed="right" header-align="center" align="center" width="150">
           <template slot-scope="scope">
-            <el-button type="text" size="small" @click="viewInfoHandle(scope.row.id)">{{ $t('info') }}</el-button>
+            <!-- <el-button type="text" size="small" @click="viewInfoHandle(scope.row.id)">{{ $t('info') }}</el-button> -->
             <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">{{ $t('update') }}</el-button>
             <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">{{ $t('delete') }}</el-button>
           </template>
